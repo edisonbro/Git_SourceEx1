6f6b4d87db8cd57a073bdb9560bc4c5676834d4b
SudhaPatilI <spiosdeveloper@gmail.com>
2015-06-16 16:38:09 +0530

    First Commit

--- Git_SourceEx1/ViewController.m
+++ Git_SourceEx1/ViewController.m
@@ -15,6 +15,7 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+    NSLog(@"First Log In Master");
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
